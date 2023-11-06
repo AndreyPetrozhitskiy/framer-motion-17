b94deacd71566672f4713eaf5034378e02fa968d
Andrey <super.okha2018@yandex.ru>
2023-11-06 17:35:10 +1000

    1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import "./style/app.scss"
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+        <div className='App__container'>
+          
+        </div>
+    </div>
+  )
+}
+
+export default App
